8d6c34efb601f4cbad9acfe7d662ca57f4b3c723
TAEHYEON <disney1026dlxogus@gmail.com>
2024-12-08 23:53:51 +0900

    db.js port 추가

--- db.js
+++ db.js
@@ -8,6 +8,7 @@ const db = async () => {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
+            port: process.env.DB_PORT,
             dateStrings: true 
         });
         return connection; 
